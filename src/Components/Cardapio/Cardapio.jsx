a3e4e45e0b269c3d1b2a5c9bdc7ccb0d3d69f4f2
adryeldeev <adryeldev@hotmail.com>
2025-07-03 17:05:53 -0300

    adicionando filtro

--- src/Components/Cardapio/Cardapio.jsx
+++ src/Components/Cardapio/Cardapio.jsx
@@ -1,42 +1,79 @@
 import React, { useEffect, useState } from 'react';
+import Filtro from '../Filtro/Filtro';
 
 const Cardapio = () => {
   const [produtos, setProdutos] = useState([]);
+  const [itemsFiltrado, setItemsFiltrado] = useState([]);
 
   useEffect(() => {
     fetch('/produtos.json')
       .then(res => res.json())
-      .then(data => setProdutos(data.produtos));
+      .then(data => {
+        setProdutos(data.produtos);
+        setItemsFiltrado(data.produtos);
+      });
   }, []);
+
+  const filtroPorHamburgueres = () => {
+    const hamburgueres = produtos.filter(produto => produto.categoria === 'Hamburguer');
+    setItemsFiltrado(hamburgueres);
+  };
+
+  const filtroPorPizza = () => {
+    const pizzas = produtos.filter(produto => produto.categoria === 'Pizza');
+    setItemsFiltrado(pizzas);
+  };
+
+  const milkShake = () => {
+    const milkShakes = produtos.filter(produto => produto.categoria === 'Milkshake');
+    setItemsFiltrado(milkShakes);
+  };
+
+  const hotDogs = () => {
+    const hotDogs = produtos.filter(produto => produto.categoria === 'Hotdog');
+    setItemsFiltrado(hotDogs);
+  };
 
   return (
     <div className="bg-gray-200 min-h-screen p-6">
       <h1 className="text-center text-2xl font-bold mb-6">Cardápio</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {produtos.map((produto) => (
-          <div
-            key={produto.id}
-            className="bg-white rounded-xl shadow-md p-4 flex flex-col items-center text-center"
-          >
-            <img
-              src={produto.imagem}
-              alt={produto.nome}
-              className="w-32 h-32 object-contain mb-4"
-            />
-            <h3 className="text-lg font-semibold text-gray-800">
-              {produto.nome}
-            </h3>
-            <span className="text-red-600 font-bold mb-2">
-              R$ {produto.preco.toFixed(2).replace('.', ',')}
-            </span>
-            <button
-              onClick={() => window.location.href = `/detalhes/${produto.id}`}
-              className="bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-4 rounded w-full transition duration-200"
+
+      <Filtro
+        filtroPorHamburgueres={filtroPorHamburgueres}
+        filtroPorPizza={filtroPorPizza}
+        milkShake={milkShake}
+        hotDogs={hotDogs}
+      />
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
+        {itemsFiltrado.length > 0 ? (
+          itemsFiltrado.map((produto) => (
+            <div
+              key={produto.id}
+              className="bg-white rounded-xl shadow-md p-4 flex flex-col items-center text-center"
             >
-              Ver Detalhes
-            </button>
-          </div>
-        ))}
+              <img
+                src={produto.imagem}
+                alt={produto.nome}
+                className="w-32 h-32 object-contain mb-4"
+              />
+              <h3 className="text-lg font-semibold text-gray-800">
+                {produto.nome}
+              </h3>
+              <span className="text-red-600 font-bold mb-2">
+                R$ {produto.preco.toFixed(2).replace('.', ',')}
+              </span>
+              <button
+                onClick={() => window.location.href = `/detalhes/${produto.id}`}
+                className="bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-4 rounded w-full transition duration-200"
+              >
+                Ver Detalhes
+              </button>
+            </div>
+          ))
+        ) : (
+          <p className="col-span-4 text-center text-gray-500">Indisponível no momento</p>
+        )}
       </div>
     </div>
   );
---
+++ src/Components/Filtro/Filtro.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { IoPizzaOutline } from "react-icons/io5";
+import { PiHamburgerBold } from "react-icons/pi";
+import { LiaHotdogSolid } from "react-icons/lia";
+import { TbMilkshake } from "react-icons/tb";
+const Filtro = ({ filtroPorPizza, filtroPorHamburgueres, milkShake, hotDogs }) => {
+  return (
+    <div className="max-w-3xl mx-auto px-1 bg-white rounded-lg shadow-md">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 text-center">
+        <button onClick={filtroPorPizza} className="flex items-center  p-4 rounded-lg transition duration-200">
+          <IoPizzaOutline className="text-4xl text-red-600 " />
+          <span className="text-gray-700">Pizzas</span>
+        </button>
+
+        <button onClick={filtroPorHamburgueres} className="flex  items-center  p-4 rounded-lg transition duration-200">
+          <PiHamburgerBold className="text-4xl text-yellow-600" />
+          <span className="text-gray-700">Hambúrgueres</span>
+        </button>
+
+        <button onClick={hotDogs} className="flex  items-center  p-4 rounded-lg transition duration-200">
+          <LiaHotdogSolid className="text-4xl text-orange-600" />
+          <span className="text-gray-700">Hot Dogs</span>
+        </button>
+
+        <button onClick={milkShake} className="flex  items-center  p-4 rounded-lg transition duration-200">
+          <TbMilkshake className="text-4xl text-pink-600" />
+          <span className="text-gray-700">Milkshakes</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+
+export default Filtro
